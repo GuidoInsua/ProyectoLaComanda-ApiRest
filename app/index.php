1d3a236d33a4945ea193a7939f3d54d9bcd8bbdc
GuidoInsua <insua.guido@gmail.com>
2024-06-02 19:27:39 -0300

    conectando BD

--- app/index.php
+++ app/index.php
@@ -1,18 +1,32 @@
 <?php
-// Guido Insua :)
-// Error Handling 
+// Error Handling
 error_reporting(-1);
 ini_set('display_errors', 1);
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
+use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Routing\RouteCollectorProxy;
+use Slim\Routing\RouteContext;
+use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+require_once './db/AccesoDatos.php';
+
+// require_once './middlewares/Logger.php';
+
+require_once './controllers/UsuarioController.php';
+
+// Load ENV
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
 
 // Instantiate App
 $app = AppFactory::create();
+
+// Set base path
+$app->setBasePath('/app');
 
 // Add error middleware
 $app->addErrorMiddleware(true, true, true);
@@ -21,26 +35,15 @@ $app->addErrorMiddleware(true, true, true);
 $app->addBodyParsingMiddleware();
 
 // Routes
-$app->get('[/]', function (Request $request, Response $response) {
-    $payload = json_encode(array('method' => 'GET', 'msg' => "Bienvenido a SlimFramework 2023"));
-    $response->getBody()->write($payload);
-    return $response->withHeader('Content-Type', 'application/json');
-});
+$app->group('/usuarios', function (RouteCollectorProxy $group) {
+    $group->get('[/]', \UsuarioController::class . ':TraerTodos');
+    //$group->get('/{usuario}', \UsuarioController::class . ':TraerUno');
+    //$group->post('[/]', \UsuarioController::class . ':CargarUno');
+  });
 
-$app->get('/test', function (Request $request, Response $response) {
-    $payload = json_encode(array('method' => 'GET', 'msg' => "Bienvenido a SlimFramework 2023"));
-    $response->getBody()->write($payload);
-    return $response->withHeader('Content-Type', 'application/json');
-});
-
-$app->post('[/]', function (Request $request, Response $response) {
-    $payload = json_encode(array('method' => 'POST', 'msg' => "Bienvenido a SlimFramework 2023"));
-    $response->getBody()->write($payload);
-    return $response->withHeader('Content-Type', 'application/json');
-});
-
-$app->post('/test', function (Request $request, Response $response) {
-    $payload = json_encode(array('method' => 'POST', 'msg' => "Bienvenido a SlimFramework 2023"));
+$app->get('[/]', function (Request $request, Response $response) {    
+    $payload = json_encode(array("mensaje" => "Slim Framework 4 PHP"));
+    
     $response->getBody()->write($payload);
     return $response->withHeader('Content-Type', 'application/json');
 });
